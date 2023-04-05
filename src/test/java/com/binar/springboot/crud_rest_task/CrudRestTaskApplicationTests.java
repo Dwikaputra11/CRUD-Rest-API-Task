819be396e1a868d5da01e7fe7d52793779abf041
Dwika <dwikariswanda@gmail.com>
2023-04-05 15:04:08 +0700

    Initial commit

---
+++ src/test/java/com/binar/springboot/crud_rest_task/CrudRestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binar.springboot.crud_rest_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
