633118735bb25f9cb60e11f86e45c731e09c1463
Dwika <dwikariswanda@gmail.com>
2023-04-06 15:22:40 +0700

    - add new comment in repos

--- src/main/java/com/binar/springboot/crud_rest_task/repos/FilmRepository.java
+++ src/main/java/com/binar/springboot/crud_rest_task/repos/FilmRepository.java
@@ -4,6 +4,8 @@ import com.binar.springboot.crud_rest_task.models.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FilmRepository extends JpaRepository<Film, Long> {
-
-
+    // find film by length > 60
+    // find film by replacementCost > 10
+    // find by Rating
+    // find by rental duration between 2 and 5
 }
