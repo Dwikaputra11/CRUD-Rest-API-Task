055f128f7363a102b8f9e3e602e6cf2adcc4a564
AbyanSS <abyansurya04@gmail.com>
2023-04-09 15:00:37 +0700

    Replacement Cost & Rating Solved

---
+++ Crud-Rest-API/src/main/java/com/binar/springboot/crud_rest_task/controller/FilmController.java
@@ -0,0 +1,66 @@
+package com.binar.springboot.crud_rest_task.controller;
+
+import com.binar.springboot.crud_rest_task.models.Film;
+import com.binar.springboot.crud_rest_task.service.FilmService;
+import com.binar.springboot.crud_rest_task.utils.ResponseHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class FilmController {
+
+    private final FilmService filmService;
+    private static final String SUCCESS_MSG = "Successfully retrieved data!";
+
+    @Autowired
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @GetMapping("/film")
+    public ResponseEntity<Object> findAll() {
+        List<Film> filmList = filmService.findAll();
+        return ResponseHandler.generateResponse(SUCCESS_MSG, HttpStatus.OK,filmList);
+    }
+
+    @GetMapping("/film/{id}")
+    public ResponseEntity<Object> findById(@PathVariable("id") int id) {
+        Film film = filmService.findById(id);
+        return ResponseHandler.generateResponse(SUCCESS_MSG, HttpStatus.OK,film);
+    }
+
+    @GetMapping("/film/save")
+    public ResponseEntity<Object> save(@RequestBody Film film){
+        filmService.save(film);
+        return ResponseHandler.generateResponse(SUCCESS_MSG, HttpStatus.OK,film);
+    }
+
+    @GetMapping("/film/update")
+    public ResponseEntity<Object> update(@RequestBody int id, Film film ) {
+        filmService.update(id, film);
+        return ResponseHandler.generateResponse(SUCCESS_MSG, HttpStatus.OK, film);
+    }
+
+    @GetMapping("/film/search")
+    public ResponseEntity<Object> findByRentalDurationRange(@RequestParam("from") int from, @RequestParam("to") int to){
+        List<Film> filmList = filmService.findByRentalDurationRange(from, to);
+        return ResponseHandler.generateResponse(SUCCESS_MSG, HttpStatus.OK,filmList);
+    }
+
+    @GetMapping("/film/findByRc")
+    public ResponseEntity<Object> findByRc(@RequestParam("r_cost") double replacementCost){
+        List<Film> filmList = filmService.findByRc(replacementCost);
+        return ResponseHandler.generateResponse(SUCCESS_MSG, HttpStatus.OK, filmList);
+    }
+
+    @GetMapping("/film/findByRating")
+    public ResponseEntity<Object> findByRating(@RequestParam("rating") String rating) {
+        List<Film> filmList = filmService.findByRating(rating);
+        return ResponseHandler.generateResponse(SUCCESS_MSG, HttpStatus.OK, filmList);
+    }
+}
---
+++ Crud-Rest-API/src/main/java/com/binar/springboot/crud_rest_task/service/FilmService.java
@@ -0,0 +1,25 @@
+package com.binar.springboot.crud_rest_task.service;
+
+import com.binar.springboot.crud_rest_task.models.Film;
+
+import java.util.List;
+
+public interface FilmService {
+
+    List<Film> findAll();
+
+    Film findById(int id);
+
+    List<Film> findByRc(double replacementCost);
+
+    List<Film> findByRating(String rating);
+
+    List<Film> findByRentalDurationRange(int from, int to);
+
+    Film save (Film film);
+
+    Film update(int id, Film updatedFilm);
+
+    void delete(int id);
+
+}
